4e84a38929f153b46d29b924bc05f2f8596ecad9
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2025-03-04 01:35:47 -0300

    feat: add root layout and define constants

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
 import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import "@/assets/styles/globals.css";
+import { APP_NAME, APP_DESCRIPTION, SERVER_URL } from "@/lib/constants";
 
 const fontRaleway = Raleway({
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "E-commerce",
-  description: "",
+  title: {
+    template: `%s | E-commerce`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
