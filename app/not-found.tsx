2f3a4d83029194e6f2f58cfe6b5f1ed95fdba636
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2025-03-06 04:29:37 -0300

    chore: update not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,8 @@
 "use client";
-
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const NotFoundPage = () => {
   return (
@@ -14,15 +14,11 @@ const NotFoundPage = () => {
         alt={`${APP_NAME} logo`}
         priority={true}
       />
-      <div className="p-6 rounded-lg shadow-md text-center">
+      <div className="p-6 w-1/3 rounded-lg shadow-md text-center dark:shadow-white/10">
         <h1 className="text-3xl font-bold mb-4">Not Found</h1>
         <p className="text-destructive">Could not find requested page</p>
-        <Button
-          variant="outline"
-          className="mt-4 ml-2"
-          onClick={() => (window.location.href = "/")}
-        >
-          Back To Home
+        <Button variant="outline" className="mt-4 ml-2" asChild>
+          <Link href="/">Back To Home</Link>
         </Button>
       </div>
     </div>
