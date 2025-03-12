fc63eba62ce02386f9a82658dfbd3e7c675c75b4
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2025-03-12 06:19:22 -0300

    feat: add dynamic cart button

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -2,14 +2,15 @@
 
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
-import { CartItem } from "@/types";
+import { Plus, Minus } from "lucide-react";
+import { Cart, CartItem } from "@/types";
 import { toast } from "sonner";
-import { addItemToCart } from "@/lib/actions/cart.actions";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
 
+  // Handle add to cart
   const handleAddToCart = async () => {
     const res = await addItemToCart(item);
 
@@ -26,7 +27,38 @@ const AddToCart = ({ item }: { item: CartItem }) => {
     });
   };
 
-  return (
+  // Handle remove from cart
+  const handleRemoveFromCart = async () => {
+    const res = await removeItemFromCart(item.productId);
+
+    if (!res.success) {
+      toast.error(res.message);
+      return;
+    }
+
+    toast(res.message, {
+      action: {
+        label: "Go to cart",
+        onClick: () => router.push("/cart"),
+      },
+    });
+  };
+
+  // Check if item is in cart
+  const existItem =
+    cart && cart.items.find((x) => x.productId === item.productId);
+
+  return existItem ? (
+    <div>
+      <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
+        <Minus className="h-4 w-4" />
+      </Button>
+      <span className="px-2">{existItem.qty}</span>
+      <Button type="button" variant="outline" onClick={handleAddToCart}>
+        <Plus className="h-4 w-4" />
+      </Button>
+    </div>
+  ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
       <Plus />
       Add to cart
