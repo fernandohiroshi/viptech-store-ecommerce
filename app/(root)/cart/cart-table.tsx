65a7f2fc567ff583f22dd11ffe58da91e48bd2b4
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2025-03-13 10:24:10 -0300

    feat: create cart page

---
+++ app/(root)/cart/cart-table.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { useTransition } from "react";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
+import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
+import { Cart } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
+
+const CartTable = ({ cart }: { cart?: Cart }) => {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  return (
+    <>
+      <h1 className="py-4 h2-bold">Sopping Cart</h1>
+      {!cart || cart.items.length === 0 ? (
+        <div>
+          cart is empty. <Link href="/">Go Shopping</Link>
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">Table</div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CartTable;
