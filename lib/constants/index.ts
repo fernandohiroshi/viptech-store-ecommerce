6205f79bb9761a19c5637ceb1121aee97f7c8c2f
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2025-04-10 23:10:01 -0300

    feat: review Prisma schema, Zod and types

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,8 +1,7 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "E-commerce";
 
 export const APP_DESCRIPTION =
-  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
-  "A modern ecommerce store built with Next.js";
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A modern ecommerce store";
 
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000/";
@@ -57,3 +56,9 @@ export const productDefaultValues = {
 export const USER_ROLES = process.env.USER_ROLES
   ? process.env.USER_ROLES.split(", ")
   : ["admin", "user"];
+
+export const reviewFormDefaultValues = {
+  title: "",
+  comment: "",
+  rating: 0,
+};
--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { string, z } from "zod";
 import {
   insertProductSchema,
   insertCartSchema,
@@ -7,6 +7,7 @@ import {
   insertOrderItemSchema,
   insertOrderSchema,
   paymentResultSchema,
+  insertReviewSchema,
 } from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
@@ -31,3 +32,8 @@ export type Order = z.infer<typeof insertOrderSchema> & {
 };
 
 export type PaymentResult = z.infer<typeof paymentResultSchema>;
+export type Review = z.infer<typeof insertReviewSchema> & {
+  id: string;
+  createdAt: Date;
+  user?: { name: string };
+};
