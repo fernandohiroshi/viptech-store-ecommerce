67d8c381815813f1717d3c1ff0f7a97fb2da3e4f
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2025-04-15 05:04:04 -0300

    feat: add icon boxes component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import IconBoxes from "@/components/icon-boxes";
 import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
 import ViewAllProductsButton from "@/components/view-all-products-button";
@@ -14,8 +15,9 @@ const HomePage = async () => {
       {featuredProducts.length > 0 && (
         <ProductCarousel data={featuredProducts} />
       )}
-      <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
+      <ProductList data={latestProducts} title="Newest Arrivals" limit={8} />
       <ViewAllProductsButton />
+      <IconBoxes />
     </>
   );
 };
--- app/admin/overview/charts.tsx
+++ app/admin/overview/charts.tsx
@@ -10,13 +10,13 @@ const Charts = ({
   return (
     <ResponsiveContainer width="100%" height={350}>
       <BarChart data={salesData}>
-        {/* Gradient for Bar
+        Gradient for Bar
         <defs>
           <linearGradient id="gradient" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="0%" stopColor="#4F46E5" /> 
-            <stop offset="100%" stopColor="#9333EA" /> 
+            <stop offset="0%" stopColor="#4F46E5" />
+            <stop offset="100%" stopColor="#9333EA" />
           </linearGradient>
-        </defs> */}
+        </defs>
         <XAxis
           dataKey="month"
           stroke="#888888"
@@ -31,13 +31,7 @@ const Charts = ({
           axisLine={false}
           tickFormatter={(value) => `${value}`}
         />
-
-        <Bar
-          dataKey="totalSales"
-          fill="currentColor"
-          radius={[4, 4, 0, 0]}
-          className="fill-primary"
-        />
+        <Bar dataKey="totalSales" fill="url(#gradient)" radius={[4, 4, 0, 0]} />
       </BarChart>
     </ResponsiveContainer>
   );
